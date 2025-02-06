7012513234c35b0c9c727021f9b0dc51eaad90e5
boilmyplate <john.maranan@proton.me>
2025-02-06 21:35:26 +1300

    fix: migrate to nvs from kickstart

--- emm-surface/configuration.nix
+++ emm-surface/configuration.nix
@@ -102,12 +102,43 @@
   programs.starship.enable = true;
   programs.starship.presets = [ "nerd-font-symbols" ];
 
-  programs.neovim = {
+  # REPLACED BY nvf
+  #programs.neovim = {
+  #  enable = true;
+  #  defaultEditor = true;
+  #  viAlias = true;
+  #  vimAlias = true;
+  #  configure = { };
+  #};
+
+  programs.nvf = {
     enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    configure = { };
+    # your settings need to go into the settings attribute set
+    # most settings are documented in the appendix
+    settings = {
+      vim = {
+        viAlias = true;
+        vimAlias = true;
+
+        theme = {
+	  enable = true;
+	  name = "gruvbox";
+	  style = "dark";
+	};
+
+        statusline.lualine.enable = true;
+	autocomplete.nvim-cmp.enable = true;
+	telescope.enable = true;
+
+        languages = {
+	  enableLSP = true;
+	  enableTreesitter = true;
+
+	  nix.enable = true;
+	};
+      };
+      
+    };
   };
 
   # Allow unfree packages
