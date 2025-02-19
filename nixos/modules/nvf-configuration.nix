2ecadfa359233d66295967b31a58cf60ed85414b
boilmyplate <john.maranan@proton.me>
2025-02-19 16:02:43 +1300

    nvf config

--- nixos/modules/nvf-configuration.nix
+++ nixos/modules/nvf-configuration.nix
@@ -1,40 +1,70 @@
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  ...
+}: {
   programs.nvf = {
     enable = true;
-    # your settings need to go into the settings attribute set
-    # most settings are documented in the appendix
     settings = {
       vim = {
         viAlias = true;
         vimAlias = true;
 
+        binds.cheatsheet.enable = true;
+        binds.whichKey.enable = true;
+
+        options = {
+          termguicolors = true;
+          shiftwidth = 4;
+          tabstop = 4;
+        };
+
         theme = {
-	  enable = true;
-	  name = "gruvbox";
-	  style = "dark";
-	}; 
+          enable = true;
+          name = "gruvbox";
+          style = "dark";
+        };
+
+        dashboard.dashboard-nvim.enable = true;
+        filetree.nvimTree.enable = true;
+        filetree.nvimTree.mappings.toggle = "\\";
+        tabline.nvimBufferline.enable = true;
+        tabline.nvimBufferline.mappings = {
+          closeCurrent = "<leader>bw";
+        };
+        tabline.nvimBufferline.setupOpts.options = {
+          separator_style = "slant";
+          indicator.style = "underline";
+          indicator.icon = "  ";
+          numbers = "none";
+        };
 
         lazy.plugins = {
-          "vim-suda" = {
+          vim-suda = {
             package = pkgs.vimPlugins.vim-suda;
           };
         };
 
         statusline.lualine.enable = true;
-	autocomplete.nvim-cmp.enable = true;
-	telescope.enable = true;
+        autocomplete.nvim-cmp.enable = true;
+        telescope.enable = true;
+
+        syntaxHighlighting = true;
+
+        terminal.toggleterm = {
+          enable = true;
+          lazygit.enable = true;
+          setupOpts.direction = "float";
+        };
 
         languages = {
-	  enableLSP = true;
-	  enableTreesitter = true;
+          enableLSP = true;
+          enableFormat = true;
+          enableTreesitter = true;
 
-	  nix.enable = true;
-	};
+          nix.enable = true;
+        };
       };
-      
     };
   };
-
 }
