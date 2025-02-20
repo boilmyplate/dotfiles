7a791226d3d55818da8408452a6417c1339bb142
boilmyplate <john.maranan@proton.me>
2025-02-21 12:30:04 +1300

    mpv config

--- nixos/home/programs/media.nix
+++ nixos/home/programs/media.nix
@@ -9,6 +9,23 @@
   programs.mpv = {
     enable = true;
     defaultProfiles = ["gpu-hq"];
-    scripts = [pkgs.mpvScripts.mpris];
+    scripts = with pkgs.mpvScripts; [ mpris modernz mpv-cheatsheet ];
+
+    config = {
+      # General
+      keep-open = "yes";
+      save-position-on-quit = "yes";
+      autofit = "60%";
+      cursor-autohide = 500;
+
+      # OSD
+      osc = "no";
+    
+      # Video
+      vo = "gpu-next";
+      gpu-api = "vulkan";
+
+      hwdec = "nvdec";
+    };
   };
 }
