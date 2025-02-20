dc9da223c95c253428ad6ec966278d9d0d2c7810
boilmyplate <john.maranan@proton.me>
2025-02-20 15:53:05 +1300

    power management

---
+++ nixos/modules/power.nix
@@ -0,0 +1,17 @@
+{
+  # Enable thermald to prevent overheating on Intel CPUs
+  services.thermald.enable = true;
+
+  # auto-cpufreq for power management
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+  };
+}
