94a942a104a7d7421802f40a355d61edc851363a
boilmyplate <john.maranan@proton.me>
2025-04-24 19:50:54 +1200

    pin pkgs

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -1,15 +1,23 @@
 { pkgs ? import <nixpkgs> {} }:
+let
+  # April 21, 2025
+  pinnedPkgs = import(builtins.fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/archive/c11863f1e964833214b767f4a369c6e6a7aba141.tar.gz";
+    sha256 = "17d9zxpqxp54gz4zv4j4g0wb8c4i29j4q76xah8zi4bh7hrp5yhr";
+  }) {};
+in
 
 pkgs.mkShell {
-  packages = with pkgs; [
+  packages = with pinnedPkgs; [
     nodejs
     python313
     jdk
     jdk17
+    php
   ];
 
 
   shellHook = ''
-    echo "nix shell baby!"
+    echo "Nix Shell active!"
   '';
 }
