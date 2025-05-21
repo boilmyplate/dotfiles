0c8a5b3572d9140c039ae8f869758c39cfa51398
boilmyplate <john.maranan@proton.me>
2025-05-22 11:17:45 +1200

    binary caching

--- nixos/modules/programs/hyprland.nix
+++ nixos/modules/programs/hyprland.nix
@@ -3,10 +3,6 @@
   pkgs,
   ...
 }: {
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  };
 
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
